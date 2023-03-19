94ca3d23208d5c85655ed022b4da0461d0692b1d
KSuzuki <98202109+kbelltree@users.noreply.github.com>
2023-03-19 16:35:51 -0700

    Fix major gotchas for the equal button

--- javascript.js
+++ javascript.js
@@ -8,6 +8,7 @@ let userInputNumbers = [];
 let operatorChosen = "";  
 let isEqualsPassed = false;
 let isOperatorSelected = false; 
+let isDigitEntry = false; 
 
 const add = function (initialValue, currentValue) {
     return initialValue + currentValue; 
@@ -51,7 +52,7 @@ const operate = function (array, operator) {
             break; 
         case "divide":
             // numbers.textContent = array.reduce(divide);
-            return array.reduce(subtract);
+            return array.reduce(divide);
             break; 
         default: 
             return;
@@ -62,12 +63,13 @@ const operate = function (array, operator) {
 // Create an eventLister for "digit" buttons that calls event.target and displays numbers as user enters them upto 10 digits 
 const displayNumEntry = function(target) {
     console.log("displayNumEntry called");
-        // Overwrite with new entry and reset isOperatorSelected to push new operator entry
-        if (numbers.textContent === "0" || isOperatorSelected){
+        // Overwrite with new entry and reset isOperatorSelected to push new operator entry, also change isEqualPassed true to false to continue calculation
+        if (numbers.textContent === "0" || isOperatorSelected || isEqualsPassed){
             numbers.textContent = target.textContent;
             isOperatorSelected = false;
-            isEqualsPassed = false; // Reset the flag when a number button is clicked
-        // Continue to enter digit
+            // isEqualsPassed = false; // Reset the flag when a number button is clicked
+            isDigitEntry = true; 
+        // Continue to enter digit if equals is not clicked 
         } else { 
         numbers.textContent += target.textContent; 
         }
@@ -85,13 +87,18 @@ const storeOperand = function() {
 
 // Create an eventListener for "equals" button that stores operand2, run operate, and display result
 const enterEquals = function() {
-    // Check if the equals button was not pressed before and there's an operator and an operand
-    if (!isEqualsPassed && userInputNumbers.length === 1){
+    // Edge: equals button is pressed more than one time, refresh for new calculation
+    if (isEqualsPassed) {
+        enterClear();
+    }
+    // Check if the equals button is not pressed before and there's an operator and an operand
+    if (!isEqualsPassed && userInputNumbers.length === 1 && isDigitEntry){
         storeOperand();
         numbers.textContent = operate(userInputNumbers, operatorChosen);
         userInputNumbers =[];
         storeOperand();
         isEqualsPassed = true;
+        isOperatorSelected = true; 
     } else {
         return;
     }
@@ -101,27 +108,29 @@ const enterEquals = function() {
 const prepareCalculation = function(target) {
     console.log("selectOperator called");
     // Edge: only update operatorChosen when operator button is clicked more than once 
-    if (isOperatorSelected) {
-        operatorChosen = target.dataset.operator;
+        if (isOperatorSelected) {
+            operatorChosen = target.dataset.operator;
+            isEqualsPassed = false; // Edge: Continue calculation after "equals" is passed 
     // IF this is the first operator button click update operatorChosen &  userInputNumbers 
-    } else { 
-        isOperatorSelected = true;
-        switch(userInputNumbers.length) {
-            case 0:
-                operatorChosen = target.dataset.operator;
-                storeOperand();
-                break; 
-            case 1: 
-                storeOperand();
-                numbers.textContent = operate(userInputNumbers, operatorChosen);
-                userInputNumbers =[];
-                storeOperand();
-                operatorChosen = target.dataset.operator;
-                break;
-            default:
-                return;  
+        } else { 
+            isOperatorSelected = true;
+            isDigitEntry = false;
+            switch(userInputNumbers.length) {
+                case 0:
+                    operatorChosen = target.dataset.operator;
+                    storeOperand();
+                    break; 
+                case 1: 
+                    storeOperand();
+                    numbers.textContent = operate(userInputNumbers, operatorChosen);
+                    userInputNumbers =[];
+                    storeOperand();
+                    operatorChosen = target.dataset.operator;
+                    break;
+                default:
+                    return;  
+            }
         }
-}
 }
 
 // Chat GPT 4 advises using delegation on eventHandler that handle digit buttons and operator buttons all in one
@@ -149,7 +158,7 @@ const enterClear = function () {
     numbers.textContent = "0";
     userInputNumbers = []; 
     operatorChosen = "";  
-    // isEqualsPassed = false;
+    isEqualsPassed = false;
     isOperatorSelected = false;         
 }
 // Attach the eventListener to clear button 
@@ -157,6 +166,13 @@ clear.addEventListener("click", enterClear);
 
 // Avoid overflow by rounding answers with long decimals
 
+// Equal Gotchas
+// Clear operatorChosen and userInputNumbers to stop recycling previous result and operator that results in buggy calculation
+// const resetOpOpd = function () {
+//     operatorChosen = "";
+//     userInputNumbers = [];
+//     isOperatorSelected = false; 
+// }
 
 // BONUS
 // Add floating point for decimals
@@ -164,6 +180,7 @@ clear.addEventListener("click", enterClear);
 // Add a backspace button to undo entries
 
 // PERSONALLY Added
+// Add "ON" button and add eventListener that activate all the eventListeners to accommodate "equals" function that removes all the eventListeners
 // IF equals is pressed but want to do calculation after that,
 // Activate the digit buttons to continue on calculation
 
